334599df8dd8d5338a3c85d154761794a3e4229f
Igor Kostyuchenok <igor@mbanq.com>
2020-09-24 12:22:03 +0200

    Better linting. Using latest packages in newly created serverless project.

--- templates/.eslintrc.js
+++ templates/.eslintrc.js
@@ -1,13 +1,13 @@
 module.exports = {
-  plugins: ["node", "security"],
+  plugins: ['node', 'security'],
   extends: [
-    "eslint:recommended",
-    "plugin:node/recommended",
-    "plugin:security/recommended"
+    'eslint:recommended',
+    'plugin:node/recommended',
+    'plugin:security/recommended'
   ],
   parser: 'babel-eslint',
   parserOptions:{
-    ecmaVersion: 2020,
+    ecmaVersion: 2020
   },
   env: {
     commonjs: true,
@@ -17,24 +17,29 @@ module.exports = {
     jest: true
   },
   rules: {
-    "no-console": 0,
-    indent: [ "error", 2 ],
-    "comma-dangle": [ "error", "never" ],
-    "comma-spacing": [ "error", { "before": false, "after": true } ],
-    "no-trailing-spaces": "error",
-    "no-multi-spaces": "error",
-    "no-multiple-empty-lines": "error",
-    "object-curly-spacing": [ "error", "always" ],
-    "template-curly-spacing": [ "error", "always" ],
-    semi: ["error", "never"],
-    eqeqeq: ["error", "always"]
+    indent: [ 'error', 2 ],
+    semi: ['error', 'never'],
+    eqeqeq: ['error', 'always'],
+    quotes: ['error', 'single', { allowTemplateLiterals: true }],
+    'no-console': 0,
+    'comma-dangle': [ 'error', 'never' ],
+    'comma-spacing': [ 'error', { before: false, after: true } ],
+    'no-trailing-spaces': 'error',
+    'no-multi-spaces': 'error',
+    'no-multiple-empty-lines': 'error',
+    'object-curly-spacing': [ 'error', 'always' ],
+    'template-curly-spacing': ['error', 'never'],
+    'no-var': 'error',
+    'quote-props': ['error', 'as-needed'],
+    'node/no-unpublished-require': ['error', {
+      allowModules: ['aws-sdk']
+    }]
   },
   overrides: [{
-    files: "**/*.test.js",
+    files: '**/*.test.js',
     rules: {
-      "node/no-unpublished-require": 0,
-      "node/no-missing-require": 0
+      'node/no-unpublished-require': 0,
+      'node/no-missing-require': 0
     }
   }]
 }
-
